f0bf8414555163e4b59290bc3f2ae10d1e722f0d
arisongha@gmail.com <arisongha@gmail.com>
2024-10-05 17:44:56 +0900

    Revert "test github actions"
    This reverts commit 101d5a49f90a1abefbf0c0eed2af4bc11e57d6fc.

--- src/main/java/com/sottie/app/gathering/application/ExitGatheringService.java
+++ src/main/java/com/sottie/app/gathering/application/ExitGatheringService.java
@@ -15,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 
