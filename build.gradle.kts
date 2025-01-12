61d5d335a3c8b4eadc63c0f6041fa12bdd579ed8
arisongha <arisongha@gmail.com>
2025-01-12 21:10:33 +0900

    logback-spring.xml 	추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
 	implementation("io.jsonwebtoken:jjwt-gson:0.12.6")
+	// slf4j
+	implementation "org.slf4j:slf4j-api:1.7.25"
 }
 
 tasks.withType<Test> {
